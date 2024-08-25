c4a45d45c42fa8d649b7f943dec2f20bc279625e
Golu_singh <637golusingh@gmail.com>
2024-08-26 01:29:39 +0530

    commit

--- routes/ProfilePicture.js
+++ routes/ProfilePicture.js
@@ -47,5 +47,28 @@ const imageUrl = canvas.toDataURL();
   res.send({ imageUrl });
 });
 
+router.get('/basedOngender/:gender', (req,res) =>{
+  const {gender} = req.params;
+  if(gender === 'male'){
+    return res.send('https://res.cloudinary.com/ducw7orvn/image/upload/v1724613750/WhatsApp_Image_2024-08-26_at_00.50.59_bdbc93fe_aydtcg.jpg');
+  }else if(gender === 'female'){
+    return res.send('https://res.cloudinary.com/ducw7orvn/image/upload/v1724613750/WhatsApp_Image_2024-08-26_at_00.51.59_fdcec28c_h4usiv.jpg');
+  }
+  else{
+    return res.send('https://res.cloudinary.com/ducw7orvn/image/upload/v1721941402/logo_dnkgj9.jpg');
+  }
+} );
+
+router.get('/basedOnProfession/:type', (req,res) =>{
+  const {gender} = req.params;
+  if(gender === 'male'){
+    return res.send('https://res.cloudinary.com/ducw7orvn/image/upload/v1724613750/WhatsApp_Image_2024-08-26_at_00.50.59_bdbc93fe_aydtcg.jpg');
+  }else if(gender === 'female'){
+    return res.send('https://res.cloudinary.com/ducw7orvn/image/upload/v1724613750/WhatsApp_Image_2024-08-26_at_00.51.59_fdcec28c_h4usiv.jpg');
+  }
+  else{
+    return res.send('https://res.cloudinary.com/ducw7orvn/image/upload/v1721941402/logo_dnkgj9.jpg');
+  }
+} );
 
 module.exports = router;
